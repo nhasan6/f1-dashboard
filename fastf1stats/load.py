980643e7c58fe95377eb5238aded241dd901ad5a
Neeya <169307919+nhasan6@users.noreply.github.com>
2026-09-08 18:05:39 -0400

    introduce COL constants from config

---
+++ fastf1stats/load.py
@@ -0,0 +1,35 @@
+from fastf1stats.config import DATA_DIR, SEASON_COLS, PITSTOP_COLS
+import pandas as pd
+
+# need to update logic so returns empty pd.dataframe(relevantcolumns)
+# printing doesnt do anything --> consider logging
+
+def load_parquet(year: int, file_name):
+    if file_name == "season":
+        output_cols = SEASON_COLS
+    elif file_name == "pitstops":
+        output_cols = PITSTOP_COLS
+    else:
+        output_cols=[]
+
+    try:
+        df = pd.read_parquet(DATA_DIR / str(year) / f"{file_name}_{year}.parquet")
+    except FileNotFoundError:
+        print("File not found")
+        return pd.DataFrame(columns=output_cols)
+    except Exception as e:
+        print(f"Error converting parquet to df: {e}")
+        return pd.DataFrame(columns=output_cols)
+    return df
+
+def load_season(year: int):
+    return load_parquet(year, "season")
+
+# def load_driver_standings(year: int):
+#     return load_parquet(year, "driver_standings")
+
+# def load_constructor_standings(year: int): 
+#     return load_parquet(year, "constructor_standings")
+
+def load_pitstops(year: int):
+    return load_parquet(year, "pitstops")
